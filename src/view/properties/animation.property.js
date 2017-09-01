ddc55ac08c2c207b1b678418adc28f12884c61f5
Eeliya Rasta <eeliya.rasta@gmail.com>
2017-09-01 18:00:50 +0200

    WIP v3

--- src/view/properties/animation.property.js
+++ src/view/properties/animation.property.js
@@ -21,17 +21,24 @@
         if (enterAnimationConfig) {
           viewNode.sequences[':enter'].next(function (done) {
             if (enterAnimationConfig.sequence) {
-              var animationMeta = AnimationMeta.GET(enterAnimationConfig.sequence);
+              var animationMeta = AnimationMeta.get(enterAnimationConfig.sequence);
+              animationMeta.duration = enterAnimationConfig.duration;
+              animationMeta.position = enterAnimationConfig.position;
+              // if (enterAnimationConfig.group) {
+              // animationMeta = animationMeta.getGroup(enterAnimationConfig.group, enterAnimationConfig.duration, enterAnimationConfig.position || '+=0');
+              // }
 
-              if (enterAnimationConfig.group) {
-                animationMeta = animationMeta.getGroup(enterAnimationConfig.group);
+              if (enterAnimationConfig.parent) {
+                console.info('gonna set parent', enterAnimationConfig.parent, 'on', enterAnimationConfig.sequence);
+                // animationMeta.timeline.pause();
+                animationMeta.setParent(AnimationMeta.get(enterAnimationConfig.parent));
               }
 
               var lastStep = enterAnimationConfig.to || enterAnimationConfig.from;
               lastStep.clearProps = 'all';
               animationMeta.add(viewNode.node, enterAnimationConfig, done);
             } else {
-              AnimationMeta.CREATE_TWEEN(viewNode.node, enterAnimationConfig, done);
+              AnimationMeta.createTween(viewNode.node, enterAnimationConfig, done);
             }
           });
         }
@@ -45,27 +52,37 @@
 
           viewNode.sequences[':leave'].next(function (done) {
             if (leaveAnimationConfig.sequence) {
-              var animationMeta = AnimationMeta.GET(leaveAnimationConfig.sequence);
-
+              var animationMeta = AnimationMeta.get(leaveAnimationConfig.sequence);
               // if the animation has order it will be added to the queue according to its order.
               // No order means lowest order
+              // debugger;
               if (typeof leaveAnimationConfig.order === 'number') {
-                animationMeta.addToQueue(leaveAnimationConfig.order, viewNode.node, function () {
-                  if (leaveAnimationConfig.group) {
-                    animationMeta = animationMeta.getGroup(leaveAnimationConfig.group);
-                  }
+                animationMeta.addToQueue(leaveAnimationConfig.order, viewNode.node, (function (viewNode, am, conf) {
+                  return function () {
+                    // if (conf.group) {
+                    //   am = am.getGroup(conf.group, conf.duration, conf.position || '+=0');
+                    // }
+                    if (conf.parent) {
+                      am.setParent(AnimationMeta.get(conf.parent));
+                    }
 
-                  animationMeta.add(viewNode.node, leaveAnimationConfig, done);
-                });
+                    am.add(viewNode.node, conf, done);
+                  };
+                })(viewNode, animationMeta, leaveAnimationConfig));
 
                 // When viewNode is the one which is destroyed, then run the queue
                 // The queue will never run if the destroyed viewNode has the lowest order
                 if (viewNode._destroyed) {
                   var finishImmediately = false;
-                  for (var key in animationMeta.queue) {
+                  while (animationMeta.parent) {
+                    animationMeta = animationMeta.parent;
+                  }
+                  var queue = animationMeta.queue;
+
+                  for (var key in queue) {
                     var item;
-                    for (var i = 0, len = animationMeta.queue[key].length; i < len; i++) {
-                      item = animationMeta.queue[key][i];
+                    for (var i = 0, len = queue[key].length; i < len; i++) {
+                      item = queue[key][i];
                       item.operation();
 
                       // If the the current queue item.node is the destroyed node, then all the animations in
@@ -92,7 +109,7 @@
 
               animationMeta.add(viewNode.node, leaveAnimationConfig, done);
             } else {
-              AnimationMeta.CREATE_TWEEN(viewNode.node, leaveAnimationConfig, done);
+              AnimationMeta.createTween(viewNode.node, leaveAnimationConfig, done);
             }
           });
         }
@@ -108,15 +125,15 @@
                   classAnimationConfig.to = Object.assign({className: '+=' + item || ''}, _config.to || {});
 
                   if (classAnimationConfig.sequence) {
-                    var animationMeta = AnimationMeta.GET(classAnimationConfig.sequence);
+                    var animationMeta = AnimationMeta.get(classAnimationConfig.sequence);
 
                     if (classAnimationConfig.group) {
-                      animationMeta = animationMeta.getGroup(classAnimationConfig.group);
+                      animationMeta = animationMeta.getGroup(classAnimationConfig.group, classAnimationConfig.duration, classAnimationConfig.position || '+=0');
                     }
 
                     animationMeta.add(viewNode.node, classAnimationConfig, done);
                   } else {
-                    AnimationMeta.CREATE_TWEEN(viewNode.node, classAnimationConfig, done);
+                    AnimationMeta.createTween(viewNode.node, classAnimationConfig, done);
                   }
                 });
               }
@@ -132,16 +149,15 @@
                   classAnimationConfig.to = {className: '-=' + item || ''};
 
                   if (classAnimationConfig.sequence) {
-                    var animationMeta = AnimationMeta.GET(classAnimationConfig.sequence);
+                    var animationMeta = AnimationMeta.get(classAnimationConfig.sequence);
 
                     if (classAnimationConfig.group) {
                       animationMeta = animationMeta.getGroup(classAnimationConfig.group);
-
                     }
 
                     animationMeta.add(viewNode.node, classAnimationConfig, done);
                   } else {
-                    AnimationMeta.CREATE_TWEEN(viewNode.node, classAnimationConfig, done);
+                    AnimationMeta.createTween(viewNode.node, classAnimationConfig, done);
                   }
                 });
               }
@@ -157,16 +173,23 @@
       autoRemoveChildren: true,
       onComplete: onComplete
     });
-    this.groups = {};
+
+    this.timeline.addLabel('beginning', 0);
+    this.duration = 0;
+    this.position = '+=0';
+    this.subSequences = {};
     this.queue = {};
-    this.commands = new Galaxy.GalaxySequence();
-    this.commands.start();
-    this.added = false;
+    // this.commands = new Galaxy.GalaxySequence();
+    // this.commands.start();
+    this.beginOffset = 0;
+    this.beginOffset2 = 0;
+    this.offset = 0;
+    this.parent = null;
   }
 
   AnimationMeta.ANIMATIONS = {};
 
-  AnimationMeta.GET = function (name) {
+  AnimationMeta.get = function (name) {
     if (!AnimationMeta.ANIMATIONS[name]) {
       AnimationMeta.ANIMATIONS[name] = new AnimationMeta();
     }
@@ -174,7 +197,11 @@
     return AnimationMeta.ANIMATIONS[name];
   };
 
-  AnimationMeta.CREATE_TWEEN = function (node, config, onComplete) {
+  AnimationMeta.parseSequence = function (sequence) {
+    return sequence.split('/').filter(Boolean);
+  };
+
+  AnimationMeta.createTween = function (node, config, onComplete) {
     var to = Object.assign({}, config.to || {});
     to.onComplete = onComplete;
     var tween = null;
@@ -199,18 +226,56 @@
     return tween;
   };
 
-  AnimationMeta.prototype.getGroup = function (name) {
-    var _this = this;
-    var group = this.groups[name];
-    if (!group) {
-      group = this.groups[name] = new AnimationMeta();
-    }
+  AnimationMeta.calculateDuration = function (duration, position) {
+    var po = position.replace('=', '');
+    return duration + Number(po);
+  };
 
-    if (!group.added && this.timeline.getChildren().indexOf(group.timeline) === -1) {
+  AnimationMeta.prototype.setParent = function (parent) {
+    var _this = this;
+    _this.parent = parent;
+    if (_this.parent.timeline.getChildren().indexOf(_this.timeline) === -1) {
+      // console.info('set parent', _this.parent.beginOffset, _this.parent.duration);
+      // If parent time line is active then add this time line at the end of the parent item time line
+      if (_this.parent.timeline.getChildren().length > 0) {
+        var cal = AnimationMeta.calculateDuration(_this.parent.duration, _this.parent.position);
+        console.info(_this.parent.beginOffset2)
+        _this.parent.beginOffset2 += cal;
+        _this.parent.timeline.add(this.timeline, 'beginning+=' + this.parent.beginOffset2);
+        _this.parent.timeline.add(function () {
+          _this.parent.beginOffset2 -= cal;
+        });
+        // _this.parent.beginOffset2 += cal;
+      } else {
+        this.parent.beginOffset += this.parent.duration;
+        this.parent.timeline.add(this.timeline, 'beginning');
+      }
+    }
+  };
+
+  AnimationMeta.prototype.getGroup = function (name, duration, position) {
+    var _this = this;
+    // var group = this.groups[name];
+    // if (!group) {
+    //   group = this.groups[name] = new AnimationMeta();
+    // }
+
+    var group = AnimationMeta.get(name);
+    group.parent = _this;
+    if (this.timeline.getChildren().indexOf(group.timeline) === -1) {
       // if the parent animation is still running, then add the group time line to the end of the parent animation
       // when and only when the parent time line has reached its end
       // group time line will be paused till the parent time line reaches its end
-      _this.timeline.add(group.timeline);
+      var calPosition = AnimationMeta.calculateDuration(duration, position);
+      group.offset++;
+      _this.timeline.add(group.timeline, 'group+=' + _this.groupPosition);
+      _this.timeline.add((function (cp) {
+        return function () {
+          group.offset--;
+          _this.groupPosition -= cp;
+        };
+      })(calPosition));
+      _this.groupPosition += calPosition;
       // if (this.timeline.progress() !== undefined) {
       //   group.timeline.pause();
       //   group.added = true;
@@ -236,17 +301,9 @@
 
   AnimationMeta.prototype.add = function (node, config, onComplete) {
     var to = Object.assign({}, config.to || {});
-
-    var tween = null;
-    // var onStart = function () {
-    //   console.info(node.offsetParent);
-    //   if (node.offsetParent === null) {
-    //     tween.progress(1);
-    //   }
-    // };
-
     to.onComplete = onComplete;
 
+    var tween = null;
     if (config.from && config.to) {
       tween = TweenLite.fromTo(node,
         config.duration || 0,
@@ -264,10 +321,19 @@
         to || {});
     }
 
-    if (this.timeline.getChildren().length > 0) {
-      this.timeline.add(tween, config.position || null);
+    var cal;
+    if (this.timeline.getChildren().length > this.offset) {
+      cal = AnimationMeta.calculateDuration(config.duration, config.position || '+=0');
+      this.beginOffset += cal;
+      this.timeline.add(tween, 'beginning+=' + this.beginOffset);
+      this.timeline.add(function () {
+        this.beginOffset -= cal;
+      });
+
     } else {
-      this.timeline.add(tween, null);
+      cal = AnimationMeta.calculateDuration(config.duration, config.position || '+=0');
+      this.beginOffset += cal;
+      this.timeline.add(tween, config.position);
     }
   };
 
@@ -277,6 +343,10 @@
    * @param {callback} operation
    */
   AnimationMeta.prototype.addToQueue = function (order, node, operation) {
+    if (this.parent) {
+      return this.parent.addToQueue(order, node, operation);
+    }
+
     if (!this.queue[order]) {
       this.queue[order] = [];
     }
