a14e47ee7e748f575e4a1eeae2c66124aecb6b2c
Eeliya <eeliya.rasta@gmail.com>
2018-08-02 01:15:51 +0200

    WIP: Improvements to animations architecture.

--- src/view/properties/animations.property.js
+++ src/view/properties/animations.property.js
@@ -290,18 +290,18 @@
     return sequence.timeline;
   };
 
-  AnimationMeta.refresh = function (timeline) {
-    const parentChildren = timeline.getChildren(false, true, true);
-    timeline.clear();
-    parentChildren.forEach(function (item) {
-      if (item.data) {
-        const conf = item.data.config;
-        timeline.add(item, conf.position);
-      } else {
-        timeline.add(item);
-      }
-    });
-  };
+  // AnimationMeta.refresh = function (timeline) {
+  //   const parentChildren = timeline.getChildren(false, true, true);
+  //   timeline.clear();
+  //   parentChildren.forEach(function (item) {
+  //     if (item.data) {
+  //       const conf = item.data.config;
+  //       timeline.add(item, conf.position);
+  //     } else {
+  //       timeline.add(item);
+  //     }
+  //   });
+  // };
 
   /**
    *
@@ -414,6 +414,8 @@
     if (_this.children.indexOf(parentNodeTimeline) === -1) {
       _this.children.push(parentNodeTimeline);
       let posInParent = childConf.positionInParent || '+=0';
+
+// debugger;
       // In the case that the parentNodeTimeline has not timeline then its _startTime should be 0
       if (parentNodeTimeline.timeline === null && children.length === 0) {
         parentNodeTimeline.pause();
@@ -424,8 +426,9 @@
           posInParent = null;
         }
       }
-
+      // parentNodeTimeline._startTime = 3;
       parentNodeTimeline.add(function () {
+        // debugger;
         _this.timeline.resume();
       }, posInParent);
     }
@@ -484,10 +487,7 @@
     viewNode.cache.animations = viewNode.cache.animations || {
       timeline: new TimelineLite({
         autoRemoveChildren: true,
-        smoothChildTiming: true,
-        onComplete: function () {
-          // viewNode.cache.animations.timeline.kill();
-        }
+        smoothChildTiming: true
       })
     };
 
@@ -506,10 +506,10 @@
       const currentProgress = sameSequenceParentTimeline.progress();
       // if the currentProgress is 0 or bigger than the nodeTimeline start time
       // then we can intercept the parentNodeTimeline
+      // debugger;
       if (nodeTimeline.startTime() < currentProgress || currentProgress === 0) {
-        sameSequenceParentTimeline.add(nodeTimeline, config.position || '+=0');
-        AnimationMeta.refresh(_this.timeline);
-        return _this.timeline.play(0);
+        _this.timeline.add(nodeTimeline, config.position || '+=0');
+        return;
       }
     }
 
